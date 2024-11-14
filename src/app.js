53c19c6961fd9f6f1d761b0997b99d0aabe6b4aa
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2024-11-13 19:16:19 -0500

    exploring routes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("hi from test");
-});
+// app.use("/user", (req, res)=>{
+//     res.send("HAHAHAHA")
+// })
 
-app.use("/hello", (req, res) => {
-    res.send("hi from hello");
-  });
+app.get("/user", (req, res)=>{
+    res.send({Fname:"ekam", Lname:"bhatia"})
+})
 
-app.use("/", (req, res) => {
-  res.send("hi from homepage2");
-});
+app.post("/user", (req, res)=>{
+    res.send("user updated successfully")
+})
 
-
+app.delete("/user", (req, res)=>{
+    res.send("user deleted successfully")
+})
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
