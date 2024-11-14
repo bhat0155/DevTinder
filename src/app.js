247d22e51b65733b63fef8482356e8349a56a777
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2024-11-14 12:37:56 -0500

    playing with next()

--- src/app.js
+++ src/app.js
@@ -2,25 +2,25 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/user", (req, res)=>{
-//     res.send("HAHAHAHA")
-// })
+app.use(
+  "/user",
+ [ (req, res, next) => {
+    console.log("1st request handler");
+    next();
 
-app.get("/user", (req, res)=>{
-    // const{userId, password}=req.query 
-    // const params=req.params
-    // console.log(params)
-    
-    res.send({Fname:"ekam", Lname:"bhatia"})
-})
-
-app.post("/user", (req, res)=>{
-    res.send("user updated successfully")
-})
-
-app.delete("/user", (req, res)=>{
-    res.send("user deleted successfully")
-})
+    // res.send("response 1");
+  },
+  (req, res, next) => {
+    console.log("2nd request handler");
+    // res.send("response 2");
+    next()
+  }],
+  (req, res, next)=>{
+    console.log("3rd request handler");
+    // res.send("response 3");
+    next()
+  }
+);
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
