ec1d9893abd15dc49071efcc49dad87c6498ebe2
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2024-11-16 12:38:05 -0500

    get api

--- src/app.js
+++ src/app.js
@@ -4,35 +4,50 @@ const app = express();
 // parse postman body
 app.use(express.json());
 
-const connectDb=require("./config/database")
-const User=require("./models/user")
+const connectDb = require("./config/database");
+const User = require("./models/user");
 
-app.post("/signup", async(req, res)=>{
-    const data={
-        firstName:"MS",
-        lastName:"Dhoni",
-        password:"MS@003"
+app.get("/profile", async (req, res) => {
+   let fName=req.body.firstName;
+   const theUser=await User.find({lastName:req.body.lastName})
+   console.log({theUser})
+    if (theUser.length===0){
+        res.status(400).send("The user does not exist")
     }
-    
    try{
-    console.log(req.body)
-    const user=req.body;
-    const newUser=new User(user)
-
-    await newUser.save();
-    res.send("user added successfully")
-   }catch(err){
+    res.send(theUser)
+   }
+   catch(err){
     res.send(err.message)
    }
-})
+});
 
-connectDb().then(()=>{
-    console.log("database connected")
+app.post("/signup", async (req, res) => {
+  const data = {
+    firstName: "MS",
+    lastName: "Dhoni",
+    password: "MS@003",
+  };
+
+  try {
+    console.log(req.body);
+    const user = req.body;
+    const newUser = new User(user);
+
+    await newUser.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+connectDb()
+  .then(() => {
+    console.log("database connected");
     app.listen(3000, () => {
-        console.log("server running on port 3000");
-      });
-}).catch((err)=>{
-    console.log(err)
-})
-
-
+      console.log("server running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
