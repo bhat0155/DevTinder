fdcdf394f0de173863a7e2748aeb73b83657ac73
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2024-11-15 20:31:10 -0500

    dynamic data in mongoose

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 const express = require("express");
 
 const app = express();
+// parse postman body
+app.use(express.json());
 
 const connectDb=require("./config/database")
 const User=require("./models/user")
 
 app.post("/signup", async(req, res)=>{
     const data={
-        firstName:"Sachin",
-        lastName:"Tendulkar",
-        password:"Sachin@003"
+        firstName:"MS",
+        lastName:"Dhoni",
+        password:"MS@003"
     }
     
-    const user=new User(data)
-    await user.save();
+   try{
+    console.log(req.body)
+    const user=req.body;
+    const newUser=new User(user)
+
+    await newUser.save();
     res.send("user added successfully")
+   }catch(err){
+    res.send(err.message)
+   }
 })
 
 connectDb().then(()=>{
