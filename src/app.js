68d870963da02570d6ab2779215f4719df51e5d6
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2024-11-16 22:00:15 -0500

    made apis

--- src/app.js
+++ src/app.js
@@ -8,18 +8,16 @@ const connectDb = require("./config/database");
 const User = require("./models/user");
 
 app.get("/profile", async (req, res) => {
-   let fName=req.body.firstName;
-   const theUser=await User.find({lastName:req.body.lastName})
-   console.log({theUser})
-    if (theUser.length===0){
-        res.status(400).send("The user does not exist")
-    }
-   try{
-    res.send(theUser)
-   }
-   catch(err){
-    res.send(err.message)
-   }
+  const theUser = await User.find({ lastName: req.body.lastName });
+  console.log({ theUser });
+  if (theUser.length === 0) {
+    res.status(400).send("The user does not exist");
+  }
+  try {
+    res.send(theUser);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 app.post("/signup", async (req, res) => {
@@ -36,6 +34,60 @@ app.post("/signup", async (req, res) => {
 
     await newUser.save();
     res.send("user added successfully");
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.delete("/delete", async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.body.id });
+
+    if (!user) {
+      res.status(404).send("The user no longer exists");
+    }
+    res.send("user deleted");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.patch("/update", async (req, res) => {
+  const whomToUpdate = await User.findByIdAndUpdate(req.body.id, req.body);
+
+  console.log(req.body);
+  if (!whomToUpdate) {
+    res.send("the user does not exist");
+  }
+
+  try {
+    await whomToUpdate.save();
+    if (whomToUpdate.length == 0) {
+      console.log("no updated user");
+    }
+    res.send("the user is updated");
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.put("/replace", async (req, res) => {
+  const data = {
+    ...req.body,
+  };
+  const replaced = await User.findOneAndReplace(
+    { firstName: req.body.firstName },
+    data,
+    { new: true }
+  );
+  console.log({ replaced });
+  if (!replaced) {
+    res.send("the person could not be found");
+  }
+
+  try {
+    await replaced.save();
+    res.send("the person is replaced");
   } catch (err) {
     res.send(err.message);
   }
